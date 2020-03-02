cc2068fd5a4aa974b62208497dc1dbc91dcb0013
bottzo <45202057+bottzo@users.noreply.github.com>
2020-03-03 00:13:34 +0100

    added the jpeg library
    it lacks the png one and all the audio ones

--- Source/main.cpp
+++ Source/main.cpp
@@ -8,23 +8,29 @@
 int main(int argc, char* args[]) {
 	pugi::xml_document Entity_doc;
 	SDL_Init(SDL_INIT_EVERYTHING);
-	int flags = IMG_INIT_JPG | IMG_INIT_PNG;
+	int flags = IMG_INIT_JPG;// | IMG_INIT_PNG;
 	int initted = IMG_Init(flags);
 	if ((initted & flags) != flags) {
 		printf("IMG_Init: Failed to init required jpg and png support!\n");
 		printf("IMG_Init: %s\n", IMG_GetError());
 		// handle error
 	}
-	flags = MIX_INIT_OGG | MIX_INIT_MOD;
+	/*flags = MIX_INIT_OGG | MIX_INIT_MOD;
 	initted = Mix_Init(flags);
 	if ((initted & flags) != flags) {
 		printf("Mix_Init: Failed to init required ogg and mod support!\n");
 		printf("Mix_Init: %s\n", Mix_GetError());
 		// handle error
-	}
+	}*/
 	if (TTF_Init() == -1) {
 		printf("TTF_Init: %s\n", TTF_GetError());
 		exit(2);
+	}
+	TTF_Font* font = NULL;
+
+	if (font == NULL)
+	{
+		printf("Could not load TTF font TTF_OpenFont: %s", TTF_GetError());
 	}
 	SDL_Quit();
 	return 0;

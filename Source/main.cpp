6f0253bbab0c7de29ee37b2776173fc07628fca8
bottzo <45202057+bottzo@users.noreply.github.com>
2020-03-06 00:54:16 +0100

    added the eastl library

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,8 +4,18 @@
 #include <SDL_Mixer.h>
 #include <SDL_ttf.h>
 #include "pugixml.hpp"
+#include "EASTL/array.h"
+#include "EASTL/iterator.h"
+
+void* __cdecl operator new[](size_t size, const char* name, int flags, unsigned debugFlags, const char* file, int line)
+{
+	return new uint8_t[size];
+}
 
 int main(int argc, char* args[]) {
+	eastl::array<int, 5> a = { 0, 1, 2, 3, 4 }; // Strict compilers such as GCC require the double brackets.
+	a[2] = 4;
+	printf("%d,%d\n", a[2],*a.begin());
 	pugi::xml_document Entity_doc;
 	SDL_Init(SDL_INIT_EVERYTHING);
 	int flags = IMG_INIT_PNG;

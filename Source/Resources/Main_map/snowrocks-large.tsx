d3064ef9eb38de45352302b0aecee40df5d45092
Dani <45202338+xsiro@users.noreply.github.com>
2020-04-06 13:24:57 +0200

    main map folder done with shape of the map

---
+++ Source/Resources/Main_map/snowrocks-large.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="snowrocks-large" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="../mindustry-raw/blocks/environment/snowrocks-large.png" width="64" height="64"/>
+</tileset>

dd22771dc57fc4963616ee97b3591d0cffcbc9f1
Dani <45202338+xsiro@users.noreply.github.com>
2020-04-07 13:39:10 +0200

    Finished a non-official main map

---
+++ Source/Resources/Main_map/edge-stencil-black.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="edge-stencil-black" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../mindustry-raw/blocks/environment/edge-stencil-black.png" width="96" height="96"/>
+</tileset>
---
+++ Source/Resources/Main_map/snowrock1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="snowrock1" tilewidth="48" tileheight="48" tilecount="1" columns="1">
+ <image source="../mindustry-raw/blocks/environment/snowrock1.png" width="48" height="48"/>
+</tileset>

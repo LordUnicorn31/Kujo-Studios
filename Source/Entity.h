1935d41754428102901cdc514c9c73404ab2773b
bottzo <45202057+bottzo@users.noreply.github.com>
2020-04-10 17:25:10 +0200

    fuixed a map bug
    some squares where displaying in black because the tileset was not correctly sized

--- Source/Entity.h
+++ Source/Entity.h
@@ -1,1 +1,65 @@
 #pragma once
+//Creo una struct entity info?
+class Entity {
+public:
+	Entity();
+	~Entity();
+	//SDL_Rect entity_rect;
+	//inagroup?/selected?
+	int health;//total health?
+	//type?
+	//state (constructing/training/collecting...)
+	//intendencia?
+	//construction time
+	//speed???
+public:
+	virtual void Draw(); 
+	virtual void Update();
+};
+
+class Building :public Entity {
+public:
+	Building();
+	~Building();
+	//state (constructing/aviable...)
+	//construction time
+	//SDL_Texture/Animation texture;
+	//SDL_Texture/Animation constructing_texture;
+	void Draw();
+	void Update();
+};
+
+//class x :public Building {};
+//Building x;
+
+class Collector :public Entity {
+public:
+	Collector();
+	~Collector();
+	//state (working/free)
+	//SDL_Texture/Animation texture;
+	//path intendencia->mina
+	//wight (capacitat que pot transportar)
+	//speed
+	void Draw();
+	void Update();
+};
+
+class CombatUnit :public Entity {
+public:
+	CombatUnit();
+	~CombatUnit();
+	//state (selected?)
+	//bool training
+	//training time
+	//SDL_Texture/Animation texture; (per cada accio)
+	//attack
+	//range
+	//cost
+	//speed
+	void Draw();
+	void Update();
+};
+
+//class y :public CombatUnit{};
+//CombatUnit a;
--- Source/EntityManager.h
+++ Source/EntityManager.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "j1Module.h"
+#include "Entity.h"
+#include "EASTL/list.h"
+
+class EntityManager :public j1Module {
+public:
+
+	EntityManager();
+
+	virtual ~EntityManager();
+
+	bool Awake(pugi::xml_node&);
+	bool Start();
+	bool PreUpdate();
+	bool Update(float dt);
+	void UpdateEntity(float dt);
+	bool PostUpdate(float dt);
+	bool CleanUp();
+
+	//Entity* CreateEntity(type);
+	//bool DestroyEntity(Entity*);
+private:
+	//eastl::list <Entity*>entities;
+	//eastl::list <Entity*>ia_entities;
+};

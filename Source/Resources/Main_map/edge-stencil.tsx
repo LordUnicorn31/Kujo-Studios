99f4dd880332fde2269f6a499305fb221201a7a8
Dani <45202338+xsiro@users.noreply.github.com>
2020-05-18 21:26:08 +0200

    .

--- Source/Resources/Main_map/edge-stencil.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="edge-stencil" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../mindustry-raw/blocks/environment/edge-stencil.png" width="96" height="96"/>
-</tileset>

94624516090c5faca6830e7fe1c3741d67ff9075
Dani <45202338+xsiro@users.noreply.github.com>
2020-05-18 21:27:49 +0200

    ..

--- Source/Resources/maps/metadata.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="metadata" tilewidth="32" tileheight="32" tilecount="2" columns="2">
- <image source="metadata.png" width="65" height="34"/>
-</tileset>

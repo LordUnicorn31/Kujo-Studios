9e3cc3c9984a8f9d3c67833cf1557e7249c3ee61
M1R4B3L <amirabelmartinez@gmail.com>
2020-06-07 23:52:13 +0200

    quest basic .h

--- Source/QuestManager.h
+++ Source/QuestManager.h
@@ -1,15 +1,47 @@
 #ifndef __QUESTMANAGER_H__
 #define __QUESTMANAGER_H__
 
+#include "Module.h"
+#include "EASTL/vector.h"
 
+class Quest
+{
+public:
+
+	Quest();
+
+	~Quest();
+
+	int questNumber = 0;
+	bool questTrigger = 0;
+
+	eastl::string questText;
+	eastl::string questDescription;
+
+	bool questCompleted = false;
+};
 
 
 class QuestManager : public Module
 {
 
-	
+public:
 
+	QuestManager();
 
+	virtual ~QuestManager();
+
+	bool Awake(pugi::xml_node& config);
+
+	bool Start();
+
+	bool Update();
+
+	bool CleanUp();
+
+	void LoadQuests();
+
+	eastl::vector<Quest*> quests;
 
 };
 
